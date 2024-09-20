a5f232bd8b47839fb1ee7ae9b161dad43d4ed915
Liudmila Umeslakkht <mila@DESKTOP-FEEKIFI>
2024-09-20 12:19:15 -0700

    added options into addcolumn migration and added first and last name into seeders

--- backend/db/migrations/20240919224447-add-firstname-lastname-to-users.js
+++ backend/db/migrations/20240919224447-add-firstname-lastname-to-users.js
@@ -1,27 +1,48 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define schema for production
+}
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-    await queryInterface.addColumn('Users', "firstName", {
+    await queryInterface.addColumn(
+      { tableName: 'Users', ...options },
+      "firstName", {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: '',
-    }),
+    });
 
-    await queryInterface.addColumn('Users', "lastName", {
+    await queryInterface.addColumn(
+      { tableName: 'Users', ...options },
+      "lastName", {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: '',
-    })
+    });
 
   },
 
   async down (queryInterface, Sequelize) {
 
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
-
+    await queryInterface.removeColumn(
+      { tableName: 'Users', ...options },
+      'firstName'
+    );
+    await queryInterface.removeColumn(
+      { tableName: 'Users', ...options }, 
+      'lastName'
+    );
   }
 };
+
+
+// async down(queryInterface, Sequelize) {
+//   options.tableName = "Users";
+//   return queryInterface.dropTable(options);
+// }
